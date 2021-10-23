ca1b5ccccb8287e4ad9c88c0f756539085542781
Jesse Brink <jbrink0925@gmail.com>
2021-10-23 13:17:17 -0400

    complete unroll.js for Springboard assessment

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,18 @@
-function unroll(squareArray) {
-
+function unroll(squareArray, newArray = []) {
+    for (let item of squareArray) {
+        if (item instanceof Array) {
+            unroll(item, newArray);
+        } else {
+            newArray.push(item)
+        };
+    };
+    return newArray;
 }
 
+const smallerSquare = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"]
+  ];
+console.log(unroll(smallerSquare));
 module.exports = unroll;
